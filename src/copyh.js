530f07dddc4bec3715d315d6a26310fc0144dd03
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-08-27 09:47:05 +0530

    Create copyh.js

---
+++ src/copyh.js
@@ -0,0 +1,9 @@
+export function copyHeaders(source, target) {
+  for (const [key, value] of Object.entries(source.headers)) {
+    try {
+      target.header(key, value);
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+}
